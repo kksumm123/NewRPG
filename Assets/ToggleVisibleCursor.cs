222663f7a46f4988b501b5238612bdefde12113e
kksumm123 <kksumm123@gmail.com>
2021-08-17 16:12:19 +0900

    CursorLock 관련 로직 수정  - Cursor.visible 삭제, CursorLock 하면 unvisible 되기때문

--- Assets/ToggleVisibleCursor.cs
+++ Assets/ToggleVisibleCursor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -8,11 +9,13 @@ public class ToggleVisibleCursor : MonoBehaviour
     public InputAction toggleButton = new InputAction("toggleKey", InputActionType.Button, "<Keyboard>/tab");
     void Start()
     {
-         
+        toggleButton.performed += TogleButton_performed;
+        toggleButton.Enable();
     }
 
-    void Update()
+    private void TogleButton_performed(InputAction.CallbackContext obj)
     {
-        
+        print("탭 눌림");
+        Cursor.lockState = Cursor.lockState == CursorLockMode.Locked ? CursorLockMode.None : CursorLockMode.Locked;
     }
 }
