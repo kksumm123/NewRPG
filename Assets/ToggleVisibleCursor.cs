17ee5688e0795c368beadd7fbc598ba23d0d0fad
kksumm123 <kksumm123@gmail.com>
2021-08-17 15:53:34 +0900

    ToggleVisibleCursor.cs 추가 및 초안 작성

---
+++ Assets/ToggleVisibleCursor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ToggleVisibleCursor : MonoBehaviour
+{
+    public InputAction toggleButton = new InputAction("toggleKey", InputActionType.Button, "<Keyboard>/tab");
+    void Start()
+    {
+         
+    }
+
+    void Update()
+    {
+        
+    }
+}
