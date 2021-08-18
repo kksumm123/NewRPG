96183ec884dbfbdfa4e9dcad1ce7744c0bd3e724
kksumm123 <kksumm123@gmail.com>
2021-08-18 15:54:46 +0900

    ChildActivator.cs 작성 완료

--- Assets/ChildActivator.cs
+++ Assets/ChildActivator.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class ChildActivator : MonoBehaviour
 {
-    
+    private void Awake()
+    {
+        // 자식 오브젝트 검색, 활성화
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(true);
+        }
+    }
 }
