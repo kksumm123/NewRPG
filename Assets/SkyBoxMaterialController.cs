b2866747acccca1aead5fdc9b8d7680b0935aa98
kksumm123 <kksumm123@gmail.com>
2021-08-18 19:51:23 +0900

    SkyBox 회전시키기 구현

---
+++ Assets/SkyBoxMaterialController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBoxMaterialController : MonoBehaviour
+{
+    [SerializeField] Material skyboxMaterial;
+    [SerializeField] float speed = 0.01f;
+    [SerializeField] float currentRotation;
+    float originRotation;
+    bool isRotation = false;
+    IEnumerator Start()
+    {
+        originRotation = skyboxMaterial.GetFloat("_Rotation");
+        isRotation = true;
+        while (isRotation)
+        {
+            currentRotation += speed * Time.deltaTime;
+            skyboxMaterial.SetFloat("_Rotation", currentRotation);
+            yield return null;
+        }
+    }
+    private void OnDestroy()
+    {
+        skyboxMaterial.SetFloat("_Rotation", originRotation);
+    }
+}
