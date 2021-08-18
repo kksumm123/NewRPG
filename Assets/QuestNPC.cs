4eb5baaf58ec2a798910fc7762a8df3f93b2bcc8
kksumm123 <kksumm123@gmail.com>
2021-08-18 21:29:39 +0900

    1. Singleton.cs 추가  - 유니티 기본 지원 https://wiki.unity3d.com/index.php/Singleton#Singleton.cs 2. TalkAlertUI.cs 완성 3. QuestNPC완성

--- Assets/QuestNPC.cs
+++ Assets/QuestNPC.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class QuestNPC : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] InputAction questAcceptKey;
+
+    void Awake()
     {
-        
+        questAcceptKey.performed += QuestAcceptKey_performed;
     }
 
-    // Update is called once per frame
-    void Update()
+    void QuestAcceptKey_performed(InputAction.CallbackContext obj)
     {
-        
+        print("퀘스트 목록 UI 표시하기");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") == false)
+            return;
+
+        print($"들어온거 누구야{other.name}");
+        questAcceptKey.Enable();
+        TalkAlertUI.Instance.ShowText("모험자야 멈춰봐!\n할 말이 있어(Q)");
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") == false)
+            return;
+
+        questAcceptKey.Disable();
+
     }
 }
---
+++ Assets/TalkAlertUI.cs
@@ -0,0 +1,26 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TalkAlertUI : Singleton<TalkAlertUI>
+{
+    Text text;
+    CanvasGroup canvasGroup;
+    void Start()
+    {
+        text = transform.Find("BG/BG/Text").GetComponent<Text>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void ShowText(string _text, float visibleTime = 3)
+    {
+        canvasGroup.alpha = 1;
+        canvasGroup.DOFade(1, 0.5f);
+
+        text.text = _text;
+
+        canvasGroup.DOFade(0, 0.5f).SetDelay(visibleTime);
+    }
+}
