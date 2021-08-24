a00b8c9081f918fa3a5e44fd637a22f0ffe272dc
kksumm123 <kksumm123@gmail.com>
2021-08-24 16:35:15 +0900

    singleton Instance 오브젝트 꺼져있어도 찾을 수 잇도록 수정

--- Assets/Singleton.cs
+++ Assets/Singleton.cs
@@ -31,6 +31,11 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
                 {
                     // Search for existing instance.
                     m_Instance = (T)FindObjectOfType(typeof(T));
+                    if (m_Instance == null)
+                    {
+                        // 비활성화 되어있는 오브젝트도 탐색
+                        m_Instance = (T)GetAllObjectsOnlyInScene<T>();
+                    }
 
                     // Create new instance if one doesn't already exist.
                     if (m_Instance == null)
@@ -48,11 +53,27 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 
                 return m_Instance;
             }
+
+            Component GetAllObjectsOnlyInScene<T1>() where T1 : Component
+            {
+                var components = Resources.FindObjectsOfTypeAll(typeof(T1));
+                foreach (UnityEngine.Object co in components)
+                {
+                    Component component = co as Component;
+                    GameObject go = component.gameObject;
+                    if (go.scene.name == null) // 씬에 있는 오브젝트가 아니므로 제외한다.
+                        continue;
+
+                    // HideFlags 이용하여 씬에 있는 오브젝트가 아닌경우 제외
+                    if (go.hideFlags == HideFlags.NotEditable || go.hideFlags == HideFlags.HideAndDontSave || go.hideFlags == HideFlags.HideInHierarchy)
+                        continue;
+
+                    return component;
+                }
+
+                return null;
+            }
         }
-    }
-    void Awake()
-    {
-        var init = Instance;
     }
 
     private void OnApplicationQuit()
