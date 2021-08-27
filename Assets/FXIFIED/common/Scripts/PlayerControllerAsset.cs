2b5475666d14beb5841cc95c18a1d29398b4b57b
kksumm123 <kksumm123@gmail.com>
2021-08-27 18:04:46 +0900

    1. 에셋 PlayerController.cs 충돌 해결 2. 에셋의 씬에서 매테리얼 URP로 업그레이드

--- Assets/FXIFIED/common/Scripts/PlayerControllerAsset.cs
+++ Assets/FXIFIED/common/Scripts/PlayerControllerAsset.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerControllerAsset : MonoBehaviour {
 
     public float Speed;
 
