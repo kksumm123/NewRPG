ac3fd73b0058ca65751d277216f51cac669d765e
kksumm123 <kksumm123@gmail.com>
2021-08-27 16:12:50 +0900

    1. 핑 찍을 수 있는 SignalIndicator.cs 완성 2. Indicator 오브젝트 추가

---
+++ Assets/SignalIndicator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignalIndicator : MonoBehaviour
+{
+    Collider terrainCollider;
+    void Awake()
+    {
+        terrainCollider = Terrain.activeTerrain.GetComponent<Collider>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && Input.GetKey(KeyCode.LeftAlt)) 
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (terrainCollider.Raycast(ray, out var hit, float.MaxValue))
+            {
+                transform.position = hit.point;
+            }
+        }
+    }
+}
