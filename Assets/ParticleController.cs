5f7e616e3d96e3a0852ac42485e0ea9b21e58348
kksumm123 <kksumm123@gmail.com>
2021-08-27 19:16:32 +0900

    1. ParticleController.cs추가, 파티클 종료되면 오브젝트 부수도록 2. Archer 변경사항 저장 및 씬 저장

---
+++ Assets/ParticleController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    ParticleSystem ps;
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        ps.Play();
+    }
+
+    void Update()
+    {
+        if (ps.isPlaying)
+            return;
+
+        // 파티클 더이상 안나온다면
+        Destroy(gameObject);
+    }
+}
