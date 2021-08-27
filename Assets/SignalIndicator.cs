e6cade192082d1b7d8aafa8ad5504a2a75b6de0b
kksumm123 <kksumm123@gmail.com>
2021-08-27 17:23:14 +0900

    기존 인디케이터 누르면 삭제하도록 1. 레이어 추가 2. 레이어할당 및 변수 설정 3. Physics.Raycast탐색 4. 맞으면 삭제

--- Assets/SignalIndicator.cs
+++ Assets/SignalIndicator.cs
@@ -6,16 +6,24 @@ public class SignalIndicator : MonoBehaviour
 {
     Collider terrainCollider;
     public GameObject indicatePrefab;
+    LayerMask indicatorLayer;
+
     void Awake()
     {
         terrainCollider = Terrain.activeTerrain.GetComponent<Collider>();
+        indicatorLayer = 1 << LayerMask.NameToLayer("Indicator");
     }
+    RaycastHit hit;
     void Update()
     {
         if (Input.GetMouseButtonDown(0) && Input.GetKey(KeyCode.LeftAlt))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (terrainCollider.Raycast(ray, out var hit, float.MaxValue))
+            if (Physics.Raycast(ray, out hit, float.MaxValue, indicatorLayer))
+            {
+                Destroy(hit.transform.gameObject);
+            }
+            else if (terrainCollider.Raycast(ray, out hit, float.MaxValue))
             {
                 //transform.position = hit.point;
                 Instantiate(indicatePrefab
