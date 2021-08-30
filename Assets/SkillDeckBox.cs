a0eb98302b64b68ee20d6b450b13b19d93cd98bd
kksumm123 <kksumm123@gmail.com>
2021-08-30 21:19:04 +0900

    1. SkillDeckBox.cs 자식들 가져오는 코드 추가 2. SkillUI Right 부분 완성

--- Assets/SkillDeckBox.cs
+++ Assets/SkillDeckBox.cs
@@ -9,10 +9,21 @@ public class SkillDeckBox : MonoBehaviour
     [SerializeField] Sprite enableSprite;
     [SerializeField] Sprite disableSprite;
     Image bgImage;
+    Text skillName;
+    Text skillLevel;
+    Image icon;
+
+
     DeckStateType deckState;
     public void Init(DeckStateType _deckState)
     {
         bgImage = transform.Find("BG").GetComponent<Image>();
+        skillName = transform.Find("SkillName").GetComponent<Text>();
+        skillLevel = transform.Find("SkillLevel").GetComponent<Text>();
+        icon = transform.Find("Icon").GetComponent<Image>();
+        skillName.enabled = false;
+        skillLevel.enabled = false;
+        icon.enabled = false;
 
         deckState = _deckState;
         bgImage.sprite = deckState == DeckStateType.Enable ?
