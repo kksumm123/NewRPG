21cefbd2a4aa5a477da618fa4094a83871c91025
kksumm123 <kksumm123@gmail.com>
2021-08-31 21:06:17 +0900

    폭포에 WaterFallScroller.cs 추가 및 작성완료

---
+++ Assets/WaterFallScroller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterFallScroller : MonoBehaviour
+{
+    new Renderer renderer;
+    [SerializeField] float speed = 0.3f;
+    Vector2 currentOffset;
+    private bool isScroller = false;
+    IEnumerator Start()
+    {
+        renderer = GetComponent<Renderer>();
+        var matOffset = currentOffset = renderer.sharedMaterial.mainTextureOffset;
+        isScroller = true;
+        while (isScroller)
+        {
+            matOffset.y += speed * Time.deltaTime;
+            renderer.sharedMaterial.mainTextureOffset = matOffset;
+            yield return null;
+        }
+    }
+    private void OnDestroy()
+    {
+        renderer.sharedMaterial.mainTextureOffset = currentOffset;
+    }
+}
