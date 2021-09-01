f1cd97a55f67a3fbc6e45c5a787eab6627426ae8
kksumm123 <kksumm123@gmail.com>
2021-09-01 16:59:49 +0900

    에디터 툴 이용해서 매테리얼들에 normalmap 적용

--- Assets/Scripts/Common/Editor/EditorOptionConfig.cs
+++ Assets/Scripts/Common/Editor/EditorOptionConfig.cs
@@ -2,10 +2,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
-
+using System;
 
 public class EditorOptionConfig : EditorWindow
 {
+    [MenuItem("Tools/Option")]
+    static void SetNormalTexture_command()
+    {
+        Debug.Log("노멀 텍스쳐 적용하자");
+        SetNormalTextures();
+    }
+
+    private static void SetNormalTextures()
+    {
+        Debug.Log("SetNormalTexture 실행");
+        foreach (var item in Selection.objects)
+        {
+            Material mat = (Material)item;
+            if (mat != null)
+                SetNormalTexture(mat);
+        }
+    }
+
+    private static void SetNormalTexture(Material mat)
+    {
+        string mainTexturePath = AssetDatabase.GetAssetPath(mat.mainTexture);
+        if (mainTexturePath != null)
+        {
+            string normalTexturePath = mainTexturePath.Replace(".png", "_n.png");
+
+            // AssetDatabase.LoadAssetAtPath 에디터상에서만 쓸 수 있는
+            // Resources 같은 기능, 위치 구애 없이 로드 가능
+            Texture normalTexture = AssetDatabase.LoadAssetAtPath<Texture>(normalTexturePath);
+            if (normalTexture != null)
+                mat.SetTexture("_BumpMap", normalTexture);
+            else
+                Debug.Log($"selected MainTex : {mainTexturePath}\nNotFound normalTex : {normalTexturePath} 경로에 파일이 없음");
+        }
+    }
+
     [MenuItem("Tools/Option")]
     static void Init()
     {
@@ -16,9 +51,13 @@ public class EditorOptionConfig : EditorWindow
     void OnGUI()
     {
         mPos = GUILayout.BeginScrollView(mPos);
-        if(GUILayout.Button("테스트 버튼"))
+        if (GUILayout.Button("테스트 버튼"))
         {
             Debug.Log("테스트 버튼 누름");
+        }
+        if (GUILayout.Button("텍스쳐에 노멀맵 달아버리기"))
+        {
+            SetNormalTexture_command();
         }
 
         for (OptionType i = OptionType.StartIndex + 1; i < OptionType.LastIndex; i++)
