bded4d227116c99ec0886e68e47e8bb0c4d9551f
kksumm123 <kksumm123@gmail.com>
2021-09-01 19:38:05 +0900

    1. 버전 2021버전으로 업그레이드 2. 다크소울 맵 매테리얼들 노멀맵 수치 1 -> 0.3

--- Assets/Scripts/Common/Editor/EditorOptionConfig.cs
+++ Assets/Scripts/Common/Editor/EditorOptionConfig.cs
@@ -6,13 +6,6 @@ using System;
 
 public class EditorOptionConfig : EditorWindow
 {
-    [MenuItem("Tools/Option")]
-    static void SetNormalTexture_command()
-    {
-        Debug.Log("노멀 텍스쳐 적용하자");
-        SetNormalTextures();
-    }
-
     private static void SetNormalTextures()
     {
         Debug.Log("SetNormalTexture 실행");
@@ -35,7 +28,10 @@ public class EditorOptionConfig : EditorWindow
             // Resources 같은 기능, 위치 구애 없이 로드 가능
             Texture normalTexture = AssetDatabase.LoadAssetAtPath<Texture>(normalTexturePath);
             if (normalTexture != null)
+            {
                 mat.SetTexture("_BumpMap", normalTexture);
+                mat.SetFloat("_BumpScale", 0.3f);
+            }
             else
                 Debug.Log($"selected MainTex : {mainTexturePath}\nNotFound normalTex : {normalTexturePath} 경로에 파일이 없음");
         }
@@ -57,7 +53,7 @@ public class EditorOptionConfig : EditorWindow
         }
         if (GUILayout.Button("텍스쳐에 노멀맵 달아버리기"))
         {
-            SetNormalTexture_command();
+            SetNormalTextures();
         }
 
         for (OptionType i = OptionType.StartIndex + 1; i < OptionType.LastIndex; i++)
